52bc0b4f71eabff6fa51838eb5554e37479de969
Dhanjyoti <dhanjyotidas00@gmail.com>
2026-04-10 02:20:03 +0530

    add https in nextjs

--- e2ee-client/src/config/axios.ts
+++ e2ee-client/src/config/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 // this proxyApi http client is for routing request through nextjs proxy 
 // so we can access cookies for auth 
 // this should be only used in the auth routes
@@ -8,7 +7,6 @@ const proxyApi = axios.create({
   baseURL: "/api",
   withCredentials: true,
 });
-
 
 
 //this client is for anything other than auth like messages conversations...
