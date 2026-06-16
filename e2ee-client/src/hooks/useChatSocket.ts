8edaf4e9aafd06d4a408a84944928bd87489ddfa
Dhanjyoti <dhanjyotidas00@gmail.com>
2026-06-16 21:06:00 +0530

    ab aur nehi

--- e2ee-client/src/hooks/useChatSocket.ts
+++ e2ee-client/src/hooks/useChatSocket.ts
@@ -2,15 +2,11 @@ import { getSocket } from "@/lib/socket/socket";
 import { useCallback } from "react";
 
 export const useChatSocket = () => {
-  const socket = getSocket();
+    const socket = getSocket();
 
-  const joinConversation = useCallback((id: string) => {
-    socket.emit("conversation:join", id);
-  }, [socket]);
+    const joinConversation = useCallback((id: string) => {
+        socket.emit("conversation:join", id);
+    }, [socket]);
 
-  const leaveConversation = useCallback((id: string) => {
-    socket.emit("conversation:leave", id);
-  }, [socket]);
-
-  return { joinConversation, leaveConversation };
+    return { joinConversation };
 };
